c9fefb8b0eb4c28afbb7e3aa0dae96ec0ec26de9
lepremiere <huick.henry@googlemail.com>
2021-03-26 23:53:34 +0100

    stable v.0.2

--- libs/worker.py
+++ libs/worker.py
@@ -11,18 +11,33 @@ from libs.buffer import ReplayBuffer
 
 class Worker(Process):
     
-    def __init__(self, name, gen, task_q, agent_in_q, batch_gen_in_q, pipe, replay_buffer, result_q, v, settings):
+    def __init__(self, name,
+                 data_gen_in_q, data_gen_out_q,
+                 task_q,
+                 agent_in_q,
+                 batch_gen_in_q,
+                 pipe,
+                 replay_buffer,
+                 news_q,
+                 val,
+                 settings):
         Process.__init__(self)
         self.name = name
         self.window_size = settings['window_size']
         self.normalization = settings['normalization']
+        self.skewed = settings['skewed']
         self.verbose = settings['verbose']
-        self.env = Environment(DataGen=gen, settings=settings)
-        self.v = v
+        self.data_gen_in_q = data_gen_in_q
+        self.data_gen_out_q = data_gen_out_q
+        self.env = Environment(in_q=self.data_gen_in_q,
+                               out_q=self.data_gen_out_q,
+                               news_q=news_q,
+                               settings=settings)
+        self.val = val
 
         # Queues and Pipe
         self.task_q = task_q
-        self.result_q = result_q
+        self.news_q = news_q
         self.agent_in_q = agent_in_q
         self.batch_gen_in_q = batch_gen_in_q
         self.replay_buffer = replay_buffer
@@ -31,8 +46,9 @@ class Worker(Process):
     def run(self):
         while True:
             next_task = self.task_q.get()
+            self.task_q.task_done()
+
             if next_task is None:
-                self.task_q.task_done()
                 break
             
             if next_task == 'play':
@@ -42,72 +58,51 @@ class Worker(Process):
                 rewards = []
                 advantages = []
                 values = []
+
                 
                 while not done:
                     self.batch_gen_in_q.put(('actions', self.name, state, 0))
                     action, _, _ = self.pipe.get()
+                    self.pipe.task_done()
                     next_state, reward, done = self.env.step(action=action, epsilon=0)
-                    self.batch_gen_in_q.put(('values', self.name, state, (action, reward, next_state, [done])))
+                    self.batch_gen_in_q.put(('values', self.name, state, (action, reward, next_state, done)))
                     _, advantage, value = self.pipe.get()
                     self.pipe.task_done()
                     
                     advantages.append(advantage)
                     values.append([value])
-                    rewards.append(reward)
+                    rewards.append([reward])
                     state = next_state
                     if not done:
                         states = (np.concatenate([states[0], [state[0]]], axis=0),
-                                np.concatenate([states[1], [state[1]]], axis=0))
-
+                                  np.concatenate([states[1], [state[1]]], axis=0))
+            
                 advantages = np.reshape(advantages, np.shape(advantages))
                 values = np.reshape(values, np.shape(values))
                 self.replay_buffer.add(states, advantages, values, np.mean(rewards))
-                if self.verbose == 2:
-                    print('Episode reward: ', round(np.mean(rewards),2))
+                
+                if self.verbose == 1:
+                    # Overwatch 
+                    message = ('Episode_end', [len(self.env.day),
+                                               round(np.mean(rewards),2),
+                                               round(np.min(rewards),2),
+                                               round(np.max(rewards),2)])
+                    self.news_q.put(message)
 
             elif next_task == 'train':
-                samples = self.replay_buffer.get_samples(skewed=False)
+         
+                if self.verbose == 1:
+                    print('Training...')
+                samples = self.replay_buffer.get_samples(skewed=self.skewed)
                 states, advantages, values = samples[0]
-                self.agent_in_q.put(('train', self.name, states, (advantages, values)))
+                self.batch_gen_in_q.put(('train', self.name, states, (advantages, values)))
+             
 
-            self.task_q.task_done()
         print('Worker:', self.name,' is done!')
-        self.v.value -=1
-
-    def play_episode(self, day):
-
-        total_rewards = []
-        trades = []
-        profit = []
-        total_reward = 0.0
-        state = self.env.reset()
-        dim = np.shape(self.env.day)[1]-1   
-        state[0] = np.reshape(np.array(state[0]), (1, self.window_size, dim))
-
-        while True:
-            batch_gen_in.put(state)
-            action = distributor_pipe.get()
-            next_state, reward, done = env.step(action, agent.epsilon)
-            next_state[0] = np.reshape(next_state[0], (1, window_size, dim))
-
-            advantage, value = agent.get_values(state, action, reward, next_state, done)
-            self.buffer.add(state, advantage, value, [reward])
-
-            total_reward += reward
-            state = next_state
-
-            if done:
-                profit.append(np.sum(env.trades['Profit']))
-                trades.append(env.trades)
-                total_rewards.append(total_reward)
-                mean_total_rewards = np.mean(total_rewards[-min(len(total_rewards), 10):])
-
-                print("Episode: ", episode+1,
-                    " Total Reward: ", total_reward,
-                    " Mean: ", mean_total_rewards,
-                    " Time: ", round(time.time()-t,2), " s")
-                break
-        return 1
+        self.val[int(self.name)] = 0
+    
+    def get_statistics(self):
+        pass
 
 if __name__ == "__main__":
     pass
--- test.py
+++ test.py
@@ -1,91 +1,30 @@
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1' 
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
 import time
-
-from libs.environment import environment
-from libs.datagen import DataGenerator
-from libs.buffer import ReplayBuffer
-from libs.agent import Agent
+from multiprocessing import JoinableQueue, Array
+from libs.controller import Controller
+from overwatch import Overwatch
 
 if __name__ == "__main__":
-    batch_size = 64
-    num_episodes = 5
-    window_size = 5
+    t = time.time()
+    settings ={'symbol': 'SP500_M1_TA',
+                'fraction': [1, 1e4],
+                'window_size': 100,
+                'num_workers': 20,
+                'buffer_size': int(1e3),
+                'buffer_batch_size': 1,
+                'normalization': False,
+                'skewed': True,
+                'training_epochs': 1,
+                'verbose': 1,
+                'start_time': t}
 
-    while True:
-        gen = DataGenerator(symbol="SP500_M1",
-                            fraction=[1, 1e4],
-                            window_size=window_size)
-        buffer = ReplayBuffer(buffer_size=int(1e5), batch_size=batch_size)
-        env = environment(DataGen=gen, normalization=False, verbose=4)
-        agent = Agent(env)
+    cycles = 100
+    schedule = [10, 2]
 
-        total_rewards = []
-        trades = []
-        t = time.time()
-        plt.ion()
-        f, (ax1,ax2) = plt.subplots(nrows=2, ncols=1, sharex=True)
-        ax1.set_title('Profit')
-        ax2.set_title('Reward')
-        profit = []
-        for episode in range(num_episodes):
-            total_reward = 0.0
-            state = env.reset()
-            dim = np.shape(env.day)[1]-1   
-            state[0] = np.reshape(np.array(state[0]), (1, window_size, dim))
-
-            while True:
-                action = agent.get_action(state)
-                next_state, reward, done = env.step(action, agent.epsilon)
-                next_state[0] = np.reshape(next_state[0], (1, window_size, dim))
-
-                advantage, value = agent.get_values(state, action, reward, next_state, done)
-                buffer.add(state, advantage, value, [reward])
-
-                total_reward += reward
-                state = next_state
-
-                if done:
-                    profit.append(np.sum(env.trades['Profit']))
-                    trades.append(env.trades)
-                    total_rewards.append(total_reward)
-                    mean_total_rewards = np.mean(total_rewards[-min(len(total_rewards), 10):])
-
-                    ax1.clear()
-                    ax1.plot(total_rewards, color='g')
-                    ax2.clear()
-                    ax2.plot(profit, color='b')
-                    ax1.axhline(0, color='k')
-                    ax2.axhline(0, color='k')
-                    plt.pause(0.05)
-                    plt.savefig('./storage/performance.png')
-
-                    print("Episode: ", episode+1,
-                        " Total Reward: ", total_reward,
-                        " Mean: ", mean_total_rewards,
-                        " Time: ", round(time.time()-t,2), " s")
-                    break
-
-            if buffer.__len__() > batch_size:
-                for _ in range(5):
-                    samples = buffer.sample(skewed=True)
-                    print(np.shape(samples), samples[0])
-   
-
-        plt.show()   
-        # trades = pd.concat(trades)
-        # long = np.array(trades['Direction'] > 0 )
-        # won = np.array(trades['Profit'] > 0)
-        # short = trades['Direction'] < 0
-
-        # print(f'\nDays/s: {round(num_episodes/(time.time()-t), 2)}')
-        # print(f'\nNumber of Trades: {len(trades)}\nTotal Profit: {np.round(np.sum(trades["Profit"]),2)} €',
-        #         f'\nAverage Return: {np.round(np.mean(trades["Profit"]),2)} ({np.round(np.min(trades["Profit"]),2)}, {np.max(trades["Profit"])})',
-        #         f'\nWon Long: {np.round(np.sum(np.sum([long, won], axis=0) == 2)/np.sum(long)*100,2)} %',
-        #         f'\nWon Short: {np.round(np.sum(np.sum([short, won], axis=0) == 2)/np.sum(long)*100,2)} %')
-        # break
-
+    val = Array('i', [1 for _ in range(settings['num_workers'])])
+    news_in_q = JoinableQueue()
+    overwatch = Overwatch(in_q=news_in_q, val=val, settings=settings).start()
+    controller = Controller(news_q=news_in_q, val=val, settings=settings)
+    controller.work(cycles, schedule)
+    controller.deinit()
+    news_in_q.close()
 
